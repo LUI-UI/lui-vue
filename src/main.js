525ef308db49931b36b78194e3f8c32a304885bb
rhmkstk <rahmi.kstk@gmail.com>
2022-01-06 14:44:30 +0300

    fix: makes link component compatable with nuxt-vue and external link

--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "remixicon/fonts/remixicon.css";
 import "./index.css";
-import router from "./router.js";
 
-createApp(App).use(router).mount("#app");
+createApp(App).mount("#app");
