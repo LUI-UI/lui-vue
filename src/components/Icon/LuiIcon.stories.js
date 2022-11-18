fa2a51744e83121c95f0d5bd3ffcba42ca87b531
rhmkstk <rahmi.kstk@gmail.com>
2022-11-18 10:09:30 +0300

    create icon story

---
+++ src/components/Icon/LuiIcon.stories.js
@@ -0,0 +1,20 @@
+import LuiIcon from "./LuiIcon.vue";
+
+export default {
+  title: "LUI/Icon",
+  component: LuiIcon,
+  // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
+};
+
+const Template = (args) => ({
+  // Components used in your story `template` are defined in the `components` object
+  components: { LuiIcon },
+  // The story's `args` need to be mapped into the template through the `setup()` method
+  setup() {
+    return { args };
+  },
+  // And then the `args` are bound to your component with `v-bind="args"`
+  template: `<lui-icon v-bind="args" />`,
+});
+export const Default = Template.bind({});
+Default.args;
