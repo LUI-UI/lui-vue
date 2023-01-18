5e8e2b026278ec4a483117e416ee89e3e6f32cbf
rhmkstk <rahmi.kstk@gmail.com>
2023-01-18 14:03:40 +0300

    create base modal styles

--- src/components/Modal/LuiModal.stories.js
+++ src/components/Modal/LuiModal.stories.js
@@ -1,20 +1,41 @@
 import LuiModal from "./LuiModal.vue";
 import { ref } from "vue";
-
+import { size } from "../../../.storybook/global-story-argtypes";
+import LuiButton from "../Button/LuiButton.vue";
 export default {
   title: "LUI/Modal",
   components: { LuiModal },
+  argTypes: {
+    size,
+  },
 };
 
 const DefaultTemplate = (args) => ({
-  components: { LuiModal },
+  components: { LuiModal, LuiButton },
   setup() {
-    let showModal = ref(false);
+    let showModal = ref(true);
     return { args, showModal };
   },
   template: `
     <button @click="showModal = true">open modal</button>
-    <lui-modal :show="showModal" @close="showModal = false"></lui-modal>
+    <lui-modal :show="showModal" v-bind="args" @close="showModal = false">
+      <div class="w-full">
+        <h2 class="text-xl font-bold mb-4">Information message</h2>
+        <p class="mb-6">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+          when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+          It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+          It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+          and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+        <div class="flex space-x-3 ml-auto">
+          <lui-button rounded color="primary">Confirm</lui-button>
+          <lui-button rounded color="secondary">Cancel</lui-button>
+        </div>
+      </div>
+    </lui-modal>
   `,
 });
 export const Default = DefaultTemplate.bind({});
+Default.args = { size: "sm" };
--- src/components/Modal/LuiModal.vue
+++ src/components/Modal/LuiModal.vue
@@ -5,18 +5,30 @@ export default {
 };
 </script>
 <script setup lang="ts">
+import { computed } from "vue";
+import type { PropType } from "vue";
 import { useId } from "../../utils/useId";
 import useFocusTrap from "../../composables/useFocusTrap";
+import type { TwClassInterface } from "@/globals/interfaces";
+import type { Size } from "@/globals/types";
+import LuiButton from "../Button/LuiButton.vue";
 const props = defineProps({
   show: {
-    type: Boolean,
+    type: Boolean as PropType<Boolean>,
     default: false,
+  },
+  showIcon: {
+    type: Boolean as PropType<Boolean>,
+    default: true,
+  },
+  size: {
+    type: String as PropType<Size>,
+    default: "sm",
   },
 });
 const emit = defineEmits(["close"]);
-console.log(props);
-console.log(emit);
 const teleportId = `lui-modal-teleport-${useId()}`;
+const modalId = `lui-modal-${useId()}`;
 const { trapRef } = useFocusTrap();
 
 function createTeleportElement() {
@@ -26,30 +38,67 @@ function createTeleportElement() {
   body[0].appendChild(teleportWrapper);
 }
 createTeleportElement();
+
+const computedModalClasses = computed(() => {
+  const classes: TwClassInterface = {
+    boxShadow: "shadow-lg",
+    borderRadius: "rounded-xl",
+    padding: "p-6",
+    width: {
+      "max-w-xs": props.size === "xs",
+      "md:max-w-sm max-w-xs": props.size === "sm",
+      "md:max-w-md max-w-xs": props.size === "md",
+      "md:max-w-lg max-w-xs": props.size === "lg",
+      "md:max-w-xl max-w-xs": props.size === "xl",
+    },
+    backgroundColor: "bg-secondary-50 dark:bg-secondary-900",
+  };
+  return Object.values(classes);
+});
+const computedOverlayClasses = computed(() => {
+  const classes: TwClassInterface = {
+    position: "fixed",
+    zIndex: "z-40",
+    top: "top-0",
+    left: "left-0",
+    width: "w-full",
+    height: "h-full",
+    display: "flex",
+    alignItems: "items-center",
+    justifyContent: "justify-center",
+    backgroundColor: "bg-secondary-900/40",
+  };
+  return Object.values(classes);
+});
 </script>
 <template>
   <Teleport :to="`#${teleportId}`">
-    <div
-      v-if="show"
-      ref="trapRef"
-      class="fixed z-20 top-0 left-0 w-full h-full flex items-center justify-center"
-    >
+    <div v-if="show" ref="trapRef" :class="computedOverlayClasses">
       <div
         role="dialog"
-        id="dialog1"
+        :id="modalId"
         aria-labelledby="dialog1_label"
         aria-modal="true"
-        class="bg-white px-10 py-8 rounded max-w-[400px]"
+        :class="computedModalClasses"
       >
+        <div v-if="showIcon" class="float-right leading-3 ml-2">
+          <LuiButton
+            variant="link"
+            color="secondary"
+            rounded
+            @click="emit('close')"
+          >
+            <span class="sr-only">close modal</span>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+              <path
+                d="M12.0007 10.5862L16.9507 5.63623L18.3647 7.05023L13.4147 12.0002L18.3647 16.9502L16.9507 18.3642L12.0007 13.4142L7.05072 18.3642L5.63672 16.9502L10.5867 12.0002L5.63672 7.05023L7.05072 5.63623L12.0007 10.5862Z"
+                fill="currentColor"
+              />
+            </svg>
+          </LuiButton>
+        </div>
+
         <slot />
-        <!-- <h1 id="dialog1_label">dialog content</h1>
-        <input placeholder="input 1" />
-        <input placeholder="input 2" />
-        <input placeholder="input 3" />
-        <div class="flex items-center space-x-4">
-          <button @click="emit('close')">Accept</button>
-          <button @click="emit('close')">Cancel</button>
-        </div> -->
       </div>
     </div>
   </Teleport>
