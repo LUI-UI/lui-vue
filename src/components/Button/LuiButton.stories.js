179821afc099011e4f0067bf3c7e1501c21f2761
rhmkstk <rahmi.kstk@gmail.com>
2023-05-08 17:07:27 +0300

    fix(button/story): remove uncessary lines

--- src/components/Button/LuiButton.stories.js
+++ src/components/Button/LuiButton.stories.js
@@ -36,12 +36,6 @@ export default {
 const Template = (args) => ({
   // Components used in your story `template` are defined in the `components` object
   components: { LuiButton },
-  // data() {
-  //   return {
-  //     is: true,
-  //   };
-  // },
-  // The story's `args` need to be mapped into the template through the `setup()` method
   setup() {
     return { args }
   },
