9c3abdbb83e7b88bfc2d2d716a5ab02530ccfd25
Feyzan Aydin <feyzanustun@gmail.com>
2023-05-17 17:31:17 +0300

    docs(luibutton): add stories for missing props and change documentation text

--- src/components/Button/LuiButton.stories.ts
+++ src/components/Button/LuiButton.stories.ts
@@ -8,19 +8,20 @@ import {
   size,
   filter
 } from '../../../.storybook/global-story-argtypes'
+
 const descriptions = {
   docs: {
     component:
-      'Button allow users to take actions, and make choices with a single tap. Also provides the style for the non-button(div,a) element look like a button'
+      'LuiButton allows the users to perform an action once activated. It also gives the non-button elements (e.g. div, a ) a style to make them look and act like a button.'
   },
   argTypes: {
-    tag: 'Sets root element tag for lui-button.(Used to make a non-button(div,a) element look like a button.)'
+    tag: 'Sets root element tag for LuiButton and is used to make a non-button element (e.g. div, a) look like a button.)'
   },
   slots: {
-    default: 'Used for the button content',
-    icon: 'Used for the icon button, can not used with the other contents',
-    prepend: 'Used for the icon on left side of text',
-    append: 'Used for the icon on right side of text'
+    default: 'Used as a default slot to place a text in a LuiButton',
+    icon: 'Used to place an icon in a LuiButton, can not be used to add text',
+    prepend: 'Used to add a text to the right side of the icon',
+    append: 'Used to add a text to the left side of the icon'
   }
 }
 const meta: Meta<typeof LuiButton> = {
@@ -59,31 +60,36 @@ const meta: Meta<typeof LuiButton> = {
   }
   // includeStories: /.*Default$/ // 👈 Storybook ignores anything that contains Data
 }
+
 export default meta
 type Story = StoryObj<typeof LuiButton>
-/** The <b>color</b> of primary button comes from the tailwind.config if you customize the primary color, default button looks with your color. */
+
+const defaultTemplate = `<lui-button>Default Button</lui-button>`
+/** This is how a default LuiButton looks. The default color is the <i>primary</i> color. <br><br>The value for the primary color is defined in the Tailwind CSS configuration file and can be changed there.*/
 export const Default: Story = {
-  render: (args) => ({
+  render: () => ({
     components: { LuiButton },
-    setup() {
-      return { args }
-    },
-    template: `<lui-button>Default button</lui-button>`
-  })
+    template: defaultTemplate
+  }),
+  parameters: {
+    docs: {
+      source: {
+        code: defaultTemplate
+      }
+    }
+  }
 }
-/** There are 4 different option to choose how to look your button. */
+
 const variantTemplate = `
 <lui-button variant="solid">Solid</lui-button>
 <lui-button variant="outline">Outline</lui-button>
 <lui-button variant="link">Link</lui-button>
 <lui-button variant="text">Text</lui-button>
 `
-export const Variants: Story = {
-  render: (args) => ({
+/** There are 4 variant options to customize how a LuiButton looks. */
+export const Variant: Story = {
+  render: () => ({
     components: { LuiButton },
-    setup() {
-      return { args }
-    },
     template: variantTemplate
   }),
   args: { variant },
@@ -95,158 +101,185 @@ export const Variants: Story = {
     }
   }
 }
-/** There are 6 color option which you can customize for buttons(See how to customize the colors: https:/www..). Also you can use the filter prop to have more colors(See the filter details:https:/www.. )  */
-export const Colors: Story = {
-  render: (args) => ({
+
+const colorTemplate = `
+<lui-button color="primary">Primary</lui-button>
+<lui-button color="secondary">Secondary</lui-button>
+<lui-button color="info">Info</lui-button>
+<lui-button color="success">Success</lui-button>
+<lui-button color="warning">Warning</lui-button>
+<lui-button color="danger">Danger</lui-button>
+`
+/** There are 6 options to customize the color of a LuiButton. (See the details: https://www...). */
+export const Color: Story = {
+  render: () => ({
     components: { LuiButton },
-    setup() {
-      const colors = ['primary', 'secondary', 'info', 'success', 'warning', 'danger']
-      return { args, colors }
-    },
-    template: `
-      <lui-button color="primary">Primary</lui-button>
-      <lui-button color="secondary">Secondary</lui-button>
-      <lui-button color="info">Info</lui-button>
-      <lui-button color="success">Success</lui-button>
-      <lui-button color="warning">Warning</lui-button>
-      <lui-button color="danger">Danger</lui-button>
-    `
+    template: colorTemplate
   }),
-  args: { color }
-}
-/** Filters provides more colors by choosing the lighter or darker tone(shade) of the selected color.*/
-export const Filters: Story = {
-  render: (args) => ({
-    components: { LuiButton },
-    setup() {
-      const colors = ['primary', 'secondary', 'info', 'success', 'warning', 'danger']
-      return { args, colors }
-    },
-    template: `
-  <div class="space-x-2 mb-4">
-    <lui-button color="primary" filter="darken">
-      Darken
-    </lui-button>
-    <lui-button color="secondary" filter="darken">
-      Darker
-    </lui-button>
-    <lui-button color="info" filter="darken">
-      Darken
-    </lui-button>
-    <lui-button color="success" filter="darken">
-      Darken
-    </lui-button>
-    <lui-button color="warning" filter="darken">
-      Darken
-    </lui-button>
-    <lui-button color="danger" filter="darken">
-      Darken
-    </lui-button>
-  </div>
-  <div class="space-x-2">
-    <lui-button color="primary" filter="lighten">
-      Lighten
-    </lui-button>
-    <lui-button color="secondary" filter="lighten">
-      Lighten
-    </lui-button>
-    <lui-button color="info" filter="lighten">
-      Lighten
-    </lui-button>
-    <lui-button color="success" filter="lighten">
-      Lighten
-    </lui-button>
-    <lui-button color="warning" filter="lighten">
-      Lighten
-    </lui-button>
-    <lui-button color="danger" filter="lighten">
-      Lighten
-    </lui-button>
-  </div>
-  `
-  }),
-  args: { filter, color }
-}
-/** There are 5 Diffent size to control button size(The size property could change padding or/and fon-size) */
-export const Sizes: Story = {
-  render: (args) => ({
-    components: { LuiButton },
-    setup() {
-      const sizes = ['xs', 'sm', 'md', 'lg', 'xl']
-      return { args, sizes }
-    },
-    template: `
-    <lui-button size="xs"> XSmall</lui-button>
-    <lui-button size="sm"> Small</lui-button>
-    <lui-button size="md"> Medium</lui-button>
-    <lui-button size="lg"> Large</lui-button>
-    <lui-button size="xl"> xLarge</lui-button>
-    `
-  }),
-  args: { size }
-}
-/** There are 3 different option to round button corners. */
-export const Rounded: Story = {
-  render: (args) => ({
-    components: { LuiButton },
-    setup() {
-      return { args }
-    },
-    template: `
-    <lui-button :rounded="false"> False </lui-button>
-    <lui-button :rounded="true"> True </lui-button>
-    <lui-button rounded="full"> Full </lui-button>
-    `
-  }),
-  args: { rounded }
-}
-/** Provides a full width button */
-export const Block: Story = {
-  render: (args) => ({
-    components: { LuiButton },
-    setup() {
-      return { args }
-    },
-    template: `<lui-button block> Block </lui-button>`
-  }),
-  args: { block }
-}
-/** To create icon button we should use the "#icon" slot in button. Lui button provides a font-size for each size, so we can inherit the font-size to use on icons. <pre><code>NOTE:text can not used with icon button! If you want to use icon with text you can use prepend or append slots</code></pre> */
-export const IconButton: Story = {
-  render: (args) => ({
-    components: { LuiButton },
-    setup() {
-      const sizes = ['xs', 'sm', 'md', 'lg', 'xl']
-      return { args, sizes }
-    },
-    template: `
-    <lui-button>
-      <template #icon>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" fill="currentColor"><path fill="none" d="M0 0h24v24H0z"/><path d="M19 21H5a1 1 0 0 1-1-1v-9H1l10.327-9.388a1 1 0 0 1 1.346 0L23 11h-3v9a1 1 0 0 1-1 1zM6 19h12V9.157l-6-5.454-6 5.454V19z"/></svg>
-      </template>
-    </lui-button>`
-  })
+  args: { color },
+  parameters: {
+    docs: {
+      source: {
+        code: colorTemplate
+      }
+    }
+  }
 }
 
-// const customTemplate = `
-// <lui-button>
-//   TEST!
-//   <template #prepend>
-//     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" fill="currentColor"><path fill="none" d="M0 0h24v24H0z"/><path d="M19 21H5a1 1 0 0 1-1-1v-9H1l10.327-9.388a1 1 0 0 1 1.346 0L23 11h-3v9a1 1 0 0 1-1 1zM6 19h12V9.157l-6-5.454-6 5.454V19z"/> </svg>
-//   </template>
-// </lui-button>
-// `
-// export const CustomTest: Story = {
-//   render: (args) => ({
-//     components: { LuiButton },
-//     setup() {
-//       return { args }
-//     },
-//     template: customTemplate
-//   }),
-//   parameters: {
-//     docs: {
-//       source: { code: customTemplate }
-//     }
-//   }
-// }
+const filterTemplate = `
+<lui-button color="primary" filter="darken">Darken</lui-button>
+<lui-button color="secondary" filter="darken">Darken</lui-button>
+<lui-button color="info" filter="darken">Darken</lui-button>
+<lui-button color="success" filter="darken">Darken</lui-button>
+<lui-button color="warning" filter="darken">Darken</lui-button>
+<lui-button color="danger" filter="darken">Darken</lui-button>
+<lui-button color="primary" filter="lighten">Lighten</lui-button>
+<lui-button color="secondary" filter="lighten">Lighten</lui-button>
+<lui-button color="info" filter="lighten">Lighten</lui-button>
+<lui-button color="success" filter="lighten">Lighten</lui-button>
+<lui-button color="warning" filter="lighten">Lighten</lui-button>
+<lui-button color="danger" filter="lighten">Lighten</lui-button>
+`
+/** The <b>filter</b> props is used to lighten or darken the selected color.*/
+export const Filter: Story = {
+  render: () => ({
+    components: { LuiButton },
+    template: `<div class="grid grid-cols-6 gap-x-2 gap-y-4">${filterTemplate}</div>`
+  }),
+  args: { filter },
+  parameters: {
+    docs: {
+      source: {
+        code: filterTemplate
+      }
+    }
+  }
+}
+
+const sizeTemplate = `
+<lui-button size="xs">xSmall</lui-button>
+<lui-button size="sm">Small</lui-button>
+<lui-button size="md">Medium</lui-button>
+<lui-button size="lg">Large</lui-button>
+<lui-button size="xl">xLarge</lui-button>
+`
+/** There are 5 options to control the size of a LuiButton. (The size property might change the padding and/or the font size.) */
+export const Size: Story = {
+  render: () => ({
+    components: { LuiButton },
+    template: sizeTemplate
+  }),
+  args: { size },
+  parameters: {
+    docs: {
+      source: {
+        code: sizeTemplate
+      }
+    }
+  }
+}
+
+const roundedTemplate = `
+<lui-button :rounded="false">False</lui-button>
+<lui-button :rounded="true">True</lui-button>
+<lui-button rounded="full">Full</lui-button>
+`
+/** There are 3 options to round the corners of a LuiButton. */
+export const Rounded: Story = {
+  render: () => ({
+    components: { LuiButton },
+    template: roundedTemplate
+  }),
+  args: { rounded },
+  parameters: {
+    docs: {
+      source: {
+        code: roundedTemplate
+      }
+    }
+  }
+}
+
+const blockTemplate = `<lui-button block>Block</lui-button>`
+/** The <b>block</b> props provides a full width button look.*/
+export const Block: Story = {
+  render: () => ({
+    components: { LuiButton },
+    template: blockTemplate
+  }),
+  args: { block },
+  parameters: {
+    docs: {
+      source: {
+        code: blockTemplate
+      }
+    }
+  }
+}
+
+// <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" fill="currentColor"><path fill="none" d="M0 0h24v24H0z"/><path d="M19 21H5a1 1 0 0 1-1-1v-9H1l10.327-9.388a1 1 0 0 1 1.346 0L23 11h-3v9a1 1 0 0 1-1 1zM6 19h12V9.157l-6-5.454-6 5.454V19z"/></svg>
+
+const iconSlotTemplate = `
+<lui-button size="xs"><template #icon><i class="ri-home-line" /></template></lui-button>
+<lui-button size="sm"><template #icon><i class="ri-home-line" /></template></lui-button>
+<lui-button size="md"><template #icon><i class="ri-home-line" /></template></lui-button>
+<lui-button size="lg"><template #icon><i class="ri-home-line" /></template></lui-button>
+<lui-button size="xl"><template #icon><i class="ri-home-line" /></template></lui-button>
+`
+
+/** To place an icon in a LuiButton the #icon slot can be used. (The icon size might be changed according to the button size.) <pre><code>NOTE: Here an icon library is used, if a svg icon was used, the font size would not be inherited. </code></pre> */
+export const IconSlot: Story = {
+  render: () => ({
+    components: { LuiButton },
+    template: `<div class="gap-x-2 gap-y-4">${iconSlotTemplate}</div>`
+  }),
+  parameters: {
+    docs: {
+      source: {
+        code: iconSlotTemplate
+      }
+    }
+  }
+}
+
+const prependSlotTemplate = `
+<lui-button>
+  <template #prepend>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" fill="currentColor"><path fill="none" d="M0 0h24v24H0z"/><path d="M19 21H5a1 1 0 0 1-1-1v-9H1l10.327-9.388a1 1 0 0 1 1.346 0L23 11h-3v9a1 1 0 0 1-1 1zM6 19h12V9.157l-6-5.454-6 5.454V19z"/> </svg>
+  </template>
+  Prepend
+</lui-button>
+`
+/** The slot #prepend is used to add a text to the right side of the icon.*/
+export const PrependSlot: Story = {
+  render: () => ({
+    components: { LuiButton },
+    template: prependSlotTemplate
+  }),
+  parameters: {
+    docs: {
+      source: { code: prependSlotTemplate }
+    }
+  }
+}
+
+const appendSlotTemplate = `
+<lui-button>
+Append
+  <template #append>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" fill="currentColor"><path fill="none" d="M0 0h24v24H0z"/><path d="M19 21H5a1 1 0 0 1-1-1v-9H1l10.327-9.388a1 1 0 0 1 1.346 0L23 11h-3v9a1 1 0 0 1-1 1zM6 19h12V9.157l-6-5.454-6 5.454V19z"/> </svg>
+  </template>
+</lui-button>
+`
+/** The slot #append is used to add a text to the left side of the icon.*/
+export const AppendSlot: Story = {
+  render: () => ({
+    components: { LuiButton },
+    template: appendSlotTemplate
+  }),
+  parameters: {
+    docs: {
+      source: { code: appendSlotTemplate }
+    }
+  }
+}
