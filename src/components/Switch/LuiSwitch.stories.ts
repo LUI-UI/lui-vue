968575062bc8fd2a4fceb397e176520daf885079
Feyzan Aydin <feyzanustun@gmail.com>
2023-05-18 17:48:46 +0300

    docs(luiswitch): add stories and fix argtypes

--- src/components/Switch/LuiSwitch.stories.ts
+++ src/components/Switch/LuiSwitch.stories.ts
@@ -15,132 +15,168 @@ const meta: Meta<typeof LuiSwitch> = {
 export default meta
 type Story = StoryObj<typeof LuiSwitch>
 
-export const Template: Story = {
-  render: (args) => ({
+const defaultTemplate = `<lui-switch />`
+/** This is how a default LuiSwitch looks. */
+export const Default: Story = {
+  render: () => ({
     components: { LuiSwitch },
-    data() {
-      return {
-        test: false,
-        names: []
+    template: defaultTemplate
+  }),
+  parameters: {
+    docs: {
+      source: {
+        code: defaultTemplate
       }
-    },
-    setup() {
-      return { args }
-    },
-    template: `
-  <div class="p-2">{{names}}</div>
-    <div class="p-2 space-x-4">
-      <lui-switch v-bind="args" id="burak" value="burak" v-model="names" />
-      <lui-switch v-bind="args" id="eser" value="eser" v-model="names" />
-      <lui-switch v-bind="args" id="fatih" value="fatih" v-model="names" />
-    </div>
-  `
-  })
-}
-export const Medium: Story = {
-  render: (args) => ({
-    components: { LuiSwitch },
-    data() {
-      return {
-        test: false,
-        names: []
-      }
-    },
-    setup() {
-      return { args }
-    },
-    template: `
-  <div class="p-2">{{names}}</div>
-    <div class="p-2 space-x-4">
-      <lui-switch v-bind="args" id="burak" value="burak" v-model="names" />
-      <lui-switch v-bind="args" id="eser" value="eser" v-model="names" />
-      <lui-switch v-bind="args" id="fatih" value="fatih" v-model="names" />
-    </div>
-  `,
-    args: {
-      size: 'md'
     }
-  })
+  }
 }
-export const Rounded: Story = {
-  render: (args) => ({
+
+const checkedTemplate = `<lui-switch checked />`
+/** The <b>checked</b> props determines if the LuiSwitch is checked or not. */
+export const Checked: Story = {
+  render: () => ({
     components: { LuiSwitch },
-    data() {
-      return {
-        test: false,
-        names: []
+    template: checkedTemplate
+  }),
+  parameters: {
+    docs: {
+      source: {
+        code: checkedTemplate
       }
-    },
-    setup() {
-      return { args }
-    },
-    template: `
-  <div class="p-2">{{names}}</div>
-    <div class="p-2 space-x-4">
-      <lui-switch v-bind="args" id="burak" value="burak" v-model="names" />
-      <lui-switch v-bind="args" id="eser" value="eser" v-model="names" />
-      <lui-switch v-bind="args" id="fatih" value="fatih" v-model="names" />
-    </div>
-  `,
-    args: {
-      rounded: true
     }
-  })
+  }
 }
+
+const disabledTemplate = `
+<lui-switch checked disabled />
+<lui-switch disabled />
+`
+/** The <b>disabled</b> props is used to disable a LuiSwitch. */
 export const Disabled: Story = {
-  render: (args) => ({
+  render: () => ({
     components: { LuiSwitch },
-    data() {
-      return {
-        test: false,
-        names: []
+    template: `<div class="space-x-4">${disabledTemplate}</div>`
+  }),
+  parameters: {
+    docs: {
+      source: {
+        code: disabledTemplate
       }
-    },
-    setup() {
-      return { args }
-    },
-    template: `
-  <div class="p-2">{{names}}</div>
-    <div class="p-2 space-x-4">
-      <lui-switch v-bind="args" id="burak" value="burak" v-model="names" />
-      <lui-switch v-bind="args" id="eser" value="eser" v-model="names" />
-      <lui-switch v-bind="args" id="fatih" value="fatih" v-model="names" />
-    </div>
-  `,
-    args: {
-      disabled: true,
-      checked: true
     }
-  })
+  }
 }
-export const Sizes: Story = {
-  render: (args) => ({
+
+const sizeTemplate = `
+<lui-switch size="xs" />
+<lui-switch size="sm" />
+<lui-switch size="md" />
+<lui-switch size="lg" />
+<lui-switch size="xl" />
+`
+/** There are 5 options to control the size of a LuiSwitch. */
+export const Size: Story = {
+  render: () => ({
     components: { LuiSwitch },
-    setup() {
-      const sizes = ['xs', 'sm', 'md', 'lg', 'xl']
-      return { args, sizes }
-    },
-    template: `
-    <div class="flex items-center space-x-8">
-      <lui-switch v-bind="args" v-for="size in sizes" :key="size" :size="size" />
-    </div>
-  `
-  })
+    args: { size },
+    template: `<div class="w-1/2 items-center grid grid-cols-5">${sizeTemplate}</div>`
+  }),
+  parameters: {
+    docs: {
+      source: {
+        code: sizeTemplate
+      }
+    }
+  }
 }
+
+const roundedTemplate = `
+<lui-switch :rounded="false" />
+<lui-switch :rounded="true" />
+<lui-switch rounded="full" />
+`
+/** There are 3 options to round the corners of a LuiSwitch. */
+export const Rounded: Story = {
+  render: () => ({
+    components: { LuiSwitch },
+    args: { rounded },
+    template: `<div class="w-1/3 grid grid-cols-3">${roundedTemplate}</div>`
+  }),
+  parameters: {
+    docs: {
+      source: {
+        code: roundedTemplate
+      }
+    }
+  }
+}
+
+const descriptionTemplate = `
+<lui-switch description="Switch description" />
+`
+/** The <b>description</b> props helps us to add a text to a LuiSwitch. */
 export const Description: Story = {
-  render: (args) => ({
+  render: () => ({
     components: { LuiSwitch },
-    setup() {
-      const sizes = ['xs', 'sm', 'md', 'lg', 'xl']
-      const states = [true, false, null, 'warning']
-      return { args, sizes, states }
-    },
-    template: `
-    <div class="flex flex-col space-y-8">
-      <div v-for="state in states" :key="state" class="flex items-center space-x-6">
-        <lui-switch v-bind="args" v-for="size in sizes" :key="size" :size="size" :state="state" description="Switch description" />
-      </div>
-    </div>
-  `
-  })
+    args: { description },
+    template: descriptionTemplate
+  }),
+  parameters: {
+    docs: {
+      source: {
+        code: descriptionTemplate
+      }
+    }
+  }
 }
+
+const stateTemplate = `
+<lui-switch :state="true" description="Switch description" />
+<lui-switch :state="false" description="Switch description" />
+<lui-switch state="warning" description="Switch description" />
+<lui-switch state="null" description="Switch description" />
+`
+/** There are 4 states to change the description color of a LuiSwitch. */
+export const State: Story = {
+  render: () => ({
+    components: { LuiSwitch },
+    args: { description },
+    template: `<div class="w-2/3 grid grid-cols-4">${stateTemplate}</div>`
+  }),
+  parameters: {
+    docs: {
+      source: {
+        code: stateTemplate
+      }
+    }
+  }
+}
+
+// const exampleTemplate = `
+// <div class="flex space-x-8">
+//   <p>Do you want to subscribe to our newsletter?</p>
+//   <lui-switch />
+// </div>
+// <div class="flex space-x-8">
+//   <p>Do you want to be notiffied by phone?</p>
+//   <lui-switch />
+// </div>
+// <div class="flex space-x-8">
+//   <p>Do you want to be notiffied by email?</p>
+//   <lui-switch />
+// </div>
+// `
+// /** Here are some use cases of a LuiSwitch. */
+// export const Examples: Story = {
+//   render: () => ({
+//     components: { LuiSwitch },
+//     args: { description },
+//     template: `<div class="flex flex-col space-x-8">${exampleTemplate}</div>`
+//   }),
+//   parameters: {
+//     docs: {
+//       source: {
+//         code: exampleTemplate
+//       }
+//     }
+//   }
+// }
