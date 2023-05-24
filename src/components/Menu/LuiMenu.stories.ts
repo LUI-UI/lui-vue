ebdffa1fd2ad92572d33999a52fd2945d17eb18e
Feyzan Aydin <feyzanustun@gmail.com>
2023-05-24 17:16:12 +0300

    docs(luimenu): add stories

--- src/components/Menu/LuiMenu.stories.ts
+++ src/components/Menu/LuiMenu.stories.ts
@@ -1,7 +1,6 @@
 import LuiMenuItem from './LuiMenuItem.vue'
 import LuiMenuDropdown from './LuiMenuDropdown.vue'
 import { color, size, rounded } from '../../../.storybook/global-story-argtypes'
-
 import type { Meta, StoryObj } from '@storybook/vue3'
 
 const meta: Meta<typeof LuiMenuDropdown> = {
@@ -31,16 +30,13 @@ export default meta
 type Story = StoryObj<typeof LuiMenuDropdown>
 
 export const Default: Story = {
-  render: (args) => ({
+  render: () => ({
     components: { LuiMenuDropdown, LuiMenuItem },
-    setup() {
-      return { args }
-    },
-    template: `<lui-menu-dropdown v-bind="args">
-             <lui-menu-item v-bind="args"> Menu Item </lui-menu-item>
-             <lui-menu-item v-bind="args"> Menu Item </lui-menu-item>
-             <lui-menu-item :disabled="true" v-bind="args"> Menu Item </lui-menu-item>
-             <lui-menu-item href="https://contentrain.io" target="_blank" v-bind="args"> 
+    template: `<lui-menu-dropdown>
+             <lui-menu-item> Menu Item </lui-menu-item>
+             <lui-menu-item> Menu Item </lui-menu-item>
+             <lui-menu-item :disabled="true"> Menu Item </lui-menu-item>
+             <lui-menu-item href="https://contentrain.io" target="_blank"> 
                 <template #prepend>
                 <i class="ri-home-line"/>
                 </template>
@@ -51,39 +47,33 @@ export const Default: Story = {
              </lui-menu-item>
             </lui-menu-dropdown>
   `,
-    args: {
-      size: 'md',
-      text: 'dropdown'
-    }
+    args: { size }
   })
 }
-export const customTriggerTemplate: Story = {
+export const CustomTrigger: Story = {
   render: (args) => ({
     components: { LuiMenuDropdown, LuiMenuItem },
     setup() {
       return { args }
     },
-    template: `<lui-menu-dropdown v-bind="args">
+    template: `<lui-menu-dropdown>
               <template #trigger="{...attrs}">
                 <div v-bind="attrs" class="w-12 h-12 rounded-full bg-primary-400 text-3xl flex items-center justify-center text-white ">
                   A
                 </div>
               </template>  
-              <lui-menu-item v-bind="args">Menu Item1</lui-menu-item>
-              <lui-menu-item v-bind="args">Menu Item2</lui-menu-item>
-              <lui-menu-item v-bind="args">Menu Item3</lui-menu-item>
-              <lui-menu-item v-bind="args">this is the test item with a huge content for a dropdown item</lui-menu-item>
+              <lui-menu-item>Menu Item1</lui-menu-item>
+              <lui-menu-item>Menu Item2</lui-menu-item>
+              <lui-menu-item>Menu Item3</lui-menu-item>
+              <lui-menu-item>this is the test item with a huge content for a dropdown item</lui-menu-item>
             </lui-menu-dropdown>
   `
   })
 }
-export const iconTemplate: Story = {
-  render: (args) => ({
+export const PrependSlot: Story = {
+  render: () => ({
     components: { LuiMenuDropdown, LuiMenuItem },
-    setup() {
-      return { args }
-    },
-    template: `<lui-menu-dropdown v-bind="args" text="Dropdown">
+    template: `<lui-menu-dropdown text="Dropdown">
               <lui-menu-item>
                 <template #prepend> <i class="ri-edit-box-line"/> </template>
                 <span>Edit</span>
@@ -106,33 +96,30 @@ export const iconTemplate: Story = {
               </lui-menu-item>
             </lui-menu-dropdown>
   `,
-    args: { size: 'md' }
+    args: { size }
   })
 }
 
-export const autoPositionTemplate: Story = {
-  render: (args) => ({
+export const AutoPosition: Story = {
+  render: () => ({
     components: { LuiMenuDropdown, LuiMenuItem },
-    setup() {
-      return { args }
-    },
     template: `
     <div class="h-[1300px]">
-      <lui-menu-dropdown v-bind="args" text="Dropdown 1">
-        <lui-menu-item v-bind="args"> Menu Item 1</lui-menu-item>
-        <lui-menu-item v-bind="args"> Menu Item 2</lui-menu-item>
-        <lui-menu-item v-bind="args"> Menu Item 3</lui-menu-item>
+      <lui-menu-dropdown text="Dropdown 1">
+        <lui-menu-item> Menu Item 1</lui-menu-item>
+        <lui-menu-item> Menu Item 2</lui-menu-item>
+        <lui-menu-item> Menu Item 3</lui-menu-item>
       </lui-menu-dropdown>
       <div class="mt-[800px]">
-        <lui-menu-dropdown v-bind="args" text="Dropdown 2">
-          <lui-menu-item v-bind="args"> Menu Item 1</lui-menu-item>
-          <lui-menu-item v-bind="args"> Menu Item 2</lui-menu-item>
-          <lui-menu-item v-bind="args"> Menu Item 3</lui-menu-item>
-          <lui-menu-item v-bind="args"> Menu Item 4</lui-menu-item>
-          <lui-menu-item v-bind="args"> Menu Item 5</lui-menu-item>
-          <lui-menu-item v-bind="args"> Menu Item 6</lui-menu-item>
-          <lui-menu-item v-bind="args"> Menu Item 7</lui-menu-item>
-          <lui-menu-item v-bind="args"> Menu Item 8</lui-menu-item>
+        <lui-menu-dropdown text="Dropdown 2">
+          <lui-menu-item> Menu Item 1</lui-menu-item>
+          <lui-menu-item> Menu Item 2</lui-menu-item>
+          <lui-menu-item> Menu Item 3</lui-menu-item>
+          <lui-menu-item> Menu Item 4</lui-menu-item>
+          <lui-menu-item> Menu Item 5</lui-menu-item>
+          <lui-menu-item> Menu Item 6</lui-menu-item>
+          <lui-menu-item> Menu Item 7</lui-menu-item>
+          <lui-menu-item> Menu Item 8</lui-menu-item>
         </lui-menu-dropdown>
       </div>
     </div>
@@ -140,8 +127,8 @@ export const autoPositionTemplate: Story = {
   })
 }
 
-export const positionsTemplate: Story = {
-  render: (args) => ({
+export const Position: Story = {
+  render: () => ({
     components: { LuiMenuDropdown, LuiMenuItem },
     setup() {
       const positions = [
@@ -154,14 +141,14 @@ export const positionsTemplate: Story = {
         'rightTop',
         'rightBottom'
       ]
-      return { args, positions }
+      return { positions }
     },
     template: `
     <div class="h-[1000px]">
       <div class="flex items-center justify-between flex-wrap mt-[300px] mx-auto max-w-[800px]">
         <div v-for="(position,i) in positions" :key="position" :class="i > 4 ? 'mt-12' : ''">
           <p>{{position}}</p>
-          <lui-menu-dropdown v-bind="args" text="Dropdown" :menu-position="position">
+          <lui-menu-dropdown text="Dropdown" :menu-position="position">
             <lui-menu-item>Menu Item position-1</lui-menu-item>
             <lui-menu-item>Menu Item position-2</lui-menu-item>
             <lui-menu-item>Menu Item position-3</lui-menu-item>
