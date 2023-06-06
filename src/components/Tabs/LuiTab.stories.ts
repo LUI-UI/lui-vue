0c065762f97bf1790cce2567e8788c455358c0c1
Feyzan Aydin <feyzanustun@gmail.com>
2023-06-06 14:47:22 +0300

    docs(luitab): add stories

--- src/components/Tabs/LuiTab.stories.ts
+++ src/components/Tabs/LuiTab.stories.ts
@@ -5,25 +5,49 @@ import LuiTabButton from './LuiTabButton.vue'
 import LuiTabPanels from './LuiTabPanels.vue'
 import LuiTabPanel from './LuiTabPanel.vue'
 import { ref } from 'vue'
+import { size } from '../../../.storybook/global-story-argtypes'
 import type { Meta, StoryObj } from '@storybook/vue3'
+
+const descriptions = {
+  docs: {
+    component:
+      'The tab feature in LUI provides an organized way to display multiple sections of content within a single view. It consists of several components, including LuiTabButton, LuiTabButtons, LuiTabPanel, LuiTabPanels and LuiTabGroup. The <b>LuiTabButtons</b> component represents the container for tab buttons, allowing users to switch between different sections of content. Each individual tab button is defined using the <b>LuiTabButton</b> component, which provides a visually clickable element for selecting a specific tab. The content for each tab is defined using the <b>LuiTabPanel</b> component, which serves as a container for the content associated with a particular tab. Multiple <b>LuiTabPanel</b> components are wrapped within the <b>LuiTabPanels</b> component, allowing for seamless management of the tabbed content. Lastly, the <b>LuiTabGroup</b> component is used to bring together the different tab-related components, providing an interactive tabbed interface. With these components, developers can easily implement a tab-based navigation system, enabling users to navigate between different sections of content with ease.'
+  },
+  argTypes: {
+    alignContent: '',
+    alignTabs: '',
+    active: '',
+    stretch: '',
+    id: '',
+    selectedIndex: '',
+    disabled: ''
+  }
+}
 
 const meta: Meta<typeof LuiTabGroup> = {
   title: 'LUI/Tab',
-  component: LuiTabGroup
-  // argTypes: {
-  //   alignTabs: {
-  //     control: 'select',
-  //     options: ['left', 'center', 'right']
-  //   },
-  //   alignContent: {
-  //     control: 'select',
-  //     options: ['left', 'center', 'right']
-  //   },
-  //   stretch: {
-  //     control: 'boolean'
-  //     // options: ["left", "center", "right"],
-  //   }
-  // }
+  component: LuiTabGroup,
+  subcomponents: { LuiTabButton, LuiTabButtons, LuiTabPanel, LuiTabPanels },
+  argTypes: {
+    size,
+    alignContent: {
+      control: 'select',
+      options: ['left', 'center', 'right'],
+      description: descriptions.argTypes.alignContent
+    },
+    disabled: { description: descriptions.argTypes.id },
+    id: { description: descriptions.argTypes.id },
+    selectedIndex: { description: descriptions.argTypes.selectedIndex },
+    alignTabs: {
+      control: 'select',
+      options: ['left', 'center', 'right']
+    },
+    active: { description: descriptions.argTypes.active },
+    stretch: {
+      control: 'boolean'
+      // options: ["left", "center", "right"],
+    }
+  }
 }
 export default meta
 type Story = StoryObj<typeof LuiTabGroup>
