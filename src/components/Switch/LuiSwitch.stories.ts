b8723e67928ea398fdaa2856e781e8562f84d3e1
Feyzan Aydin <feyzanustun@gmail.com>
2023-06-06 11:33:29 +0300

    docs(luiswitch): add stories

--- src/components/Switch/LuiSwitch.stories.ts
+++ src/components/Switch/LuiSwitch.stories.ts
@@ -58,7 +58,7 @@ export const Checked: Story = {
         code: checkedTemplate
       },
       description: {
-        story: 'The <b>checked</b> props determines if the LuiSwitch is checked or not.'
+        story: 'The <b>checked</b> prop determines if the LuiSwitch is checked or not.'
       }
     }
   }
@@ -79,7 +79,7 @@ export const Disabled: Story = {
         code: disabledTemplate
       },
       description: {
-        story: 'The <b>disabled</b> props is used to disable a LuiSwitch.'
+        story: 'The <b>disabled</b> prop is used to disable a LuiSwitch.'
       }
     }
   }
@@ -104,7 +104,8 @@ export const Size: Story = {
         code: sizeTemplate
       },
       description: {
-        story: 'There are 5 options to control the size of a LuiSwitch.'
+        story:
+          "The <b>size</b> prop in the LuiSwitch provides five options to control the size of the switch. These options include 'xs', 'sm', 'md', 'lg' and 'xl', allowing you to choose the desired size for the switch."
       }
     }
   }
@@ -149,7 +150,7 @@ export const Description: Story = {
       },
       description: {
         story:
-          'The <b>description</b> props is used to add a description to a LuiSwitch, typically used for validation and error handling.'
+          'The <b>description</b> prop is used to add a description to a LuiSwitch, typically used for validation and error handling.'
       }
     }
   }
@@ -174,7 +175,7 @@ export const State: Story = {
       },
       description: {
         story:
-          'There are 4 states used for validation and error handling and they give a border of the state color to the LuiSwitch. When used with the <b>description</b> props they also change the color of the description.'
+          'There are 4 states used for validation and error handling and they give a border of the state color to the LuiSwitch. When used with the <b>description</b> prop they also change the color of the description.'
       }
     }
   }
