1ec2858bdd6eebff0616e0c575456563e21be566
rhmkstk <rahmi.kstk@gmail.com>
2023-06-12 13:18:52 +0300

    fix(overlay): change overlay component to compatible modals
    #122

--- src/components/Overlay/LuiOverlay.stories.ts
+++ src/components/Overlay/LuiOverlay.stories.ts
@@ -16,13 +16,39 @@ export const Default: Story = {
     components: { LuiOverlay, LuiSelect, LuiInput },
     setup() {
       const showOverlay = ref(false)
-      return { args, showOverlay }
+      const buttonOverlay = ref(false)
+      // function createTeleportElement() {
+      //   const body = document.getElementsByTagName('body')
+      //   const teleportWrapper = document.createElement('div')
+      //   teleportWrapper.setAttribute('id', '')
+      //   body[0].appendChild(teleportWrapper)
+      // }
+      // if (typeof window !== 'undefined') {
+      //   createTeleportElement()
+      // }
+      return { args, buttonOverlay, showOverlay }
     },
     template: `
       <div class="text-4xl font-bold">test</div>
-      <lui-overlay :show="showOverlay" wrap-page overlay-style="opacity" color="primary" opacity="xl" @close="showOverlay = false">
-        <LuiSelect :options="['Bayhan','Serkan','Rahmi','Can','Feyzan']" @click="showOverlay = true" />
+      <lui-overlay :show="showOverlay" overlay-style="opacity" rounded="sm" @close="showOverlay = false">
+        <div class="relative z-10 max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <a href="#">
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+          </a>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+          <button @click="showOverlay = true" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Read more
+              <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+          </button>
+        </div>
+        <template #overlay>
+          <button @click="showOverlay = true" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Close
+          <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+          </button>
+        </template>
       </lui-overlay>
+      
    `
   })
 }
@@ -34,7 +60,11 @@ export const WrapPage: Story = {
       return { args, showOverlay }
     },
     template: `
-    <lui-overlay :show="showOverlay" @close="showOverlay = false">
+    <button @click="showOverlay = true" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+      Read more
+      <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+    </button>
+    <lui-overlay :show="showOverlay" overlay-style="opacity" fixed @close="showOverlay = false">
       <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
@@ -45,14 +75,7 @@ export const WrapPage: Story = {
             <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
         </button>
       </div>
-      <template #overlay>
-      <button @click="showOverlay = true" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-        Close overlay
-        <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-      </button>
-      </template>
     </lui-overlay>
-
    `
   })
 }
--- src/components/Overlay/LuiOverlay.vue
+++ src/components/Overlay/LuiOverlay.vue
@@ -9,13 +9,12 @@ import type { PropType } from 'vue'
 import type { Color, Size } from '@/globals/types'
 import { computed } from 'vue'
 import type { TwClassInterface } from '@/globals/interfaces'
-type OverlayStyleType = 'blur' | 'opacity'
 const props = defineProps({
   show: {
     type: Boolean as PropType<Boolean>,
     default: false
   },
-  wrapPage: {
+  fixed: {
     type: Boolean as PropType<Boolean>,
     default: false
   },
@@ -31,9 +30,17 @@ const props = defineProps({
     type: String as PropType<Size>,
     default: 'xs'
   },
-  overlayStyle: {
-    type: String as PropType<OverlayStyleType>,
-    default: 'blur'
+  rounded: {
+    type: String as PropType<Size | 'none'>,
+    default: 'none'
+  },
+  disableBlur: {
+    type: Boolean as PropType<Boolean>,
+    default: false
+  },
+  noCenter: {
+    type: Boolean as PropType<Boolean>,
+    default: false
   }
 })
 const emit = defineEmits(['close'])
@@ -41,44 +48,72 @@ const emit = defineEmits(['close'])
 function triggerClose() {
   emit('close')
 }
+const computedWrapperClasses = computed(() => {
+  const classes: TwClassInterface = {
+    position: props.fixed ? 'fixed' : 'relative',
+    inset: props.fixed ? 'inset-0' : '',
+    zIndex: props.fixed ? 'z-50' : '',
+    overflow: props.fixed ? 'overflow-hidden' : '',
+    width: props.fixed ? 'w-full' : 'w-max',
+    height: props.fixed ? 'h-full' : 'h-max'
+  }
+  return Object.values({ ...classes })
+})
 const computedOverlayClasses = computed(() => {
   const classes: TwClassInterface = {
-    position: props.wrapPage ? 'fixed' : 'absolute',
-    backgroundColor: {
-      [`bg-${props.color}-900/30`]: props.opacity === 'xs',
-      [`bg-${props.color}-900/40`]: props.opacity === 'sm',
-      [`bg-${props.color}-900/50`]: props.opacity === 'md',
-      [`bg-${props.color}-900/60`]: props.opacity === 'lg',
-      [`bg-${props.color}-900/70`]: props.opacity === 'xl'
+    position: props.fixed ? 'fixed' : 'absolute',
+    inset: props.fixed ? 'inset-0' : '',
+    overflow: props.fixed ? 'overflow-auto' : '',
+    width: 'w-full',
+    height: 'h-full',
+    zIndex: 'z-50',
+    display: 'flex',
+    alignItems: 'items-center',
+    justifyContent: 'justify-center',
+    borderRadius: {
+      'rounded-md': props.rounded === 'xs',
+      'rounded-lg': props.rounded === 'sm',
+      'rounded-xl': props.rounded === 'md',
+      'rounded-2xl': props.rounded === 'lg',
+      'rounded-3xl': props.rounded === 'xl',
+      '': props.rounded === 'none'
     },
-    backdropBlur: {
-      'backdrop-blur-sm': props.blur === 'xs',
-      'backdrop-blur': props.blur === 'sm',
-      'backdrop-blur-md': props.blur === 'md',
-      'backdrop-blur-lg': props.blur === 'lg',
-      'backdrop-blur-xl': props.blur === 'xl'
-    }
+    backgroundColor: props.disableBlur
+      ? {
+          [`bg-${props.color}-900/30`]: props.opacity === 'xs',
+          [`bg-${props.color}-900/40`]: props.opacity === 'sm',
+          [`bg-${props.color}-900/50`]: props.opacity === 'md',
+          [`bg-${props.color}-900/60`]: props.opacity === 'lg',
+          [`bg-${props.color}-900/70`]: props.opacity === 'xl'
+        }
+      : '',
+    backdropBlur: !props.disableBlur
+      ? {
+          'backdrop-blur-sm': props.blur === 'xs',
+          'backdrop-blur': props.blur === 'sm',
+          'backdrop-blur-md': props.blur === 'md',
+          'backdrop-blur-lg': props.blur === 'lg',
+          'backdrop-blur-xl': props.blur === 'xl'
+        }
+      : ''
   }
-  return props.overlayStyle === 'blur'
-    ? [classes.position, classes.backdropBlur]
-    : [classes.position, classes.backgroundColor]
+  return Object.values({ ...classes })
 })
 </script>
 <template>
   <div
-    class="overlay-wrapper"
-    :class="wrapPage ? 'fixed z-50' : 'w-max relative'"
+    v-if="fixed ? show : true"
     @click="triggerClose"
     @keydown.esc="triggerClose"
+    :class="computedWrapperClasses"
   >
-    <div v-if="show" class="overlay inset-0 w-full h-full" :class="computedOverlayClasses"></div>
-    <div @click.stop class="slot-wrapper w-max"><slot /></div>
-    <div
-      v-if="show && !wrapPage"
-      @click.stop
-      class="slot-wrapper absolute m-auto w-max h-max inset-0 z-10"
-    >
+    <div v-if="!fixed && show" :class="computedOverlayClasses">
       <slot name="overlay" />
+    </div>
+    <div :class="fixed ? computedOverlayClasses : ''">
+      <div @click.stop>
+        <slot />
+      </div>
     </div>
   </div>
 </template>
