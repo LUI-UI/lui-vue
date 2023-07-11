2a7c942bb33dcc3dc7c1a51c02cf9f860c935e5a
rhmkstk <rahmi.kstk@gmail.com>
2023-07-11 15:13:28 +0300

    fix(popover): fix popover storybook

--- src/components/Popover/LuiPopover.stories.ts
+++ src/components/Popover/LuiPopover.stories.ts
@@ -1,47 +1,13 @@
 import type { Meta, StoryObj } from '@storybook/vue3'
-import { color, filter, rounded, size, variant } from '../../../.storybook/global-story-argtypes'
 import LuiPopover from './LuiPopover.vue'
 
 // const sizeList = size.options
 // const colorList = color.options
 // const filterList = filter.options
 
-const descriptions = {
-  docs: {
-    component:
-      'The LuiTag allows users to navigate between different sections or views within a container. It provides a visually appealing and intuitive interface for organizing and accessing content, supporting both horizontal and vertical layouts with customizable styling and event handling.',
-  },
-  argTypes: {
-    prepend: 'Used to place an icon to the left side of the component',
-    default: 'Used as default to add a text in the component',
-    closeIcon: 'Specifies whether the tag should display a close icon.',
-  },
-}
-
 const meta: Meta<typeof LuiPopover> = {
   title: 'LUI/LuiPopover',
   component: LuiPopover,
-  argTypes: {
-    variant,
-    color,
-    size,
-    filter,
-    rounded,
-    closeIcon: {
-      control: 'boolean',
-      options: [true, false],
-      default: true,
-      description: descriptions.argTypes.closeIcon,
-    },
-    prepend: { description: descriptions.argTypes.prepend },
-    default: { description: descriptions.argTypes.default },
-  },
-  decorators: [() => ({ template: '<div class="space-x-2"><story/></div>' })],
-  parameters: {
-    docs: {
-      description: { component: descriptions.docs.component },
-    },
-  },
 }
 export default meta
 
