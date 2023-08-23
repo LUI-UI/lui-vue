313d798a9bca20b180ca9fb65b9598c6cddd663e
rhmkstk <rahmi.kstk@gmail.com>
2023-08-23 15:10:23 +0300

    fix(focustrap): change focus-trap composable with the vue-use focusTrap component
    #188

--- src/components/Modal/LuiModal.vue
+++ src/components/Modal/LuiModal.vue
@@ -8,8 +8,10 @@ export default {
 <script setup lang="ts">
 import { computed, watch } from 'vue'
 import type { PropType } from 'vue'
+import { UseFocusTrap } from '@vueuse/integrations/useFocusTrap/component'
 import { useId } from '../../utils/useId'
-import useFocusTrap from '../../composables/useFocusTrap'
+
+// import useFocusTrap from '../../composables/useFocusTrap'
 import LuiButton from '../Button/LuiButton.vue'
 import type { TwClassInterface } from '@/globals/interfaces'
 import type { Size } from '@/globals/types'
@@ -43,7 +45,7 @@ const props = defineProps({
 const emit = defineEmits(['close'])
 const teleportId = `lui-modal-teleport-${useId()}`
 const modalId = `lui-modal-${useId()}`
-const { trapRef } = useFocusTrap()
+// const { trapRef } = useFocusTrap()
 
 function createTeleportElement() {
   const body = document.getElementsByTagName('body')
@@ -132,30 +134,32 @@ const computedModalClasses = computed(() => {
 
 <template>
   <Teleport :to="`#${teleportId}`">
-    <div v-if="show" ref="trapRef" class="fixed inset-0 z-50 overflow-hidden">
-      <div class="dialog-wrapper" :class="computedDialogWrapperClasses">
-        <div
-          :id="modalId"
-          role="dialog"
-          aria-labelledby="lui-modal"
-          aria-modal="true"
-          :class="computedModalClasses"
-          v-bind="$attrs"
-        >
-          <div v-if="showIcon" class="float-right leading-3 ml-2">
-            <LuiButton variant="link" color="secondary" rounded @click="emit('close')">
-              <span class="sr-only">close modal</span>
-              <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
-                <path
-                  d="M12.0007 10.5862L16.9507 5.63623L18.3647 7.05023L13.4147 12.0002L18.3647 16.9502L16.9507 18.3642L12.0007 13.4142L7.05072 18.3642L5.63672 16.9502L10.5867 12.0002L5.63672 7.05023L7.05072 5.63623L12.0007 10.5862Z"
-                  fill="currentColor"
-                />
-              </svg>
-            </LuiButton>
+    <UseFocusTrap v-if="show" :options="{ immediate: true }">
+      <div class="fixed inset-0 z-50 overflow-hidden">
+        <div class="dialog-wrapper" :class="computedDialogWrapperClasses">
+          <div
+            :id="modalId"
+            role="dialog"
+            aria-labelledby="lui-modal"
+            aria-modal="true"
+            :class="computedModalClasses"
+            v-bind="$attrs"
+          >
+            <div v-if="showIcon" class="float-right leading-3 ml-2">
+              <LuiButton variant="link" color="secondary" rounded @click="emit('close')">
+                <span class="sr-only">close modal</span>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+                  <path
+                    d="M12.0007 10.5862L16.9507 5.63623L18.3647 7.05023L13.4147 12.0002L18.3647 16.9502L16.9507 18.3642L12.0007 13.4142L7.05072 18.3642L5.63672 16.9502L10.5867 12.0002L5.63672 7.05023L7.05072 5.63623L12.0007 10.5862Z"
+                    fill="currentColor"
+                  />
+                </svg>
+              </LuiButton>
+            </div>
+            <slot />
           </div>
-          <slot />
         </div>
       </div>
-    </div>
+    </UseFocusTrap>
   </Teleport>
 </template>
