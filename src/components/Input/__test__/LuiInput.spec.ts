515b8818f351ba810775e0e8545beafadbb87083
yusikerem <kerem.yky@gmail.com>
2023-10-30 12:03:13 +0300

    test(luiinput): add tests for LuiInput
    arrange some tests

--- src/components/Input/__test__/LuiInput.spec.ts
+++ src/components/Input/__test__/LuiInput.spec.ts
@@ -44,71 +44,71 @@ describe('Default LuiInput', () => {
     expect(wrapper.props()).toEqual(expectedDefaultProps)
   })
 })
-describe('LuiInput class tests', () => {
-  let wrapper: VueWrapper<any>
-  beforeEach(() => {
-    wrapper = mount(LuiInput)
-  })
-  afterEach(() => {
-    wrapper.unmount()
-  })
-  it('renders correctly when rounded prop is false', async () => {
-    await wrapper.setProps({ rounded: false })
-    expect(wrapper.classes()).not.contain('rounded')
-    expect(wrapper.classes()).not.contain('rounded-lg')
-    expect(wrapper.classes()).not.contain('rounded-full')
-  })
+// describe('LuiInput class tests', () => {
+//   let wrapper: VueWrapper<any>
+//   beforeEach(() => {
+//     wrapper = mount(LuiInput)
+//   })
+//   afterEach(() => {
+//     wrapper.unmount()
+//   })
+//   it('renders correctly when rounded prop is false', async () => {
+//     await wrapper.setProps({ rounded: false })
+//     expect(wrapper.classes()).not.contain('rounded')
+//     expect(wrapper.classes()).not.contain('rounded-lg')
+//     expect(wrapper.classes()).not.contain('rounded-full')
+//   })
 
-  it('renders correctly when rounded prop is true', async () => {
-    await wrapper.setProps({ rounded: true })
-    expect(wrapper.find('input').classes()).contain('rounded-lg')
-  })
-  it('renders correctly when rounded prop is full', async () => {
-    await wrapper.setProps({ rounded: 'full' })
-    expect(wrapper.find('input').classes()).contain('rounded-full')
-  })
+//   it('renders correctly when rounded prop is true', async () => {
+//     await wrapper.setProps({ rounded: true })
+//     expect(wrapper.find('input').classes()).contain('rounded-lg')
+//   })
+//   it('renders correctly when rounded prop is full', async () => {
+//     await wrapper.setProps({ rounded: 'full' })
+//     expect(wrapper.find('input').classes()).contain('rounded-full')
+//   })
 
-  it('provides full width when block prop is given', async () => {
-    await wrapper.setProps({ block: true })
-    expect(wrapper.classes()).toContain('w-full')
-  })
-  it('renders sizes properly', async () => {
-    const sizesAndClasses = {
-      xs: {
-        text: 'xs',
-        px: 1.5,
-        py: 1,
-      },
-      sm: {
-        text: 'sm',
-        px: 2,
-        py: 1.5,
-      },
-      md: {
-        text: 'base',
-        px: 2.5,
-        py: 2,
-      },
-      lg: {
-        text: 'lg',
-        px: 2.5,
-        py: 2.5,
-      },
-      xl: {
-        text: 'xl',
-        px: 3,
-        py: 3.5,
-      },
-    }
-    for (const [key, value] of Object.entries(sizesAndClasses)) {
-      await wrapper.setProps({ size: key })
-      const classes = wrapper.find('[data-testid="lui-input-input"]').classes()
-      expect(classes).toContain(`px-${value.px}`)
-      expect(classes).toContain(`py-${value.py}`)
-      expect(classes).toContain(`text-${value.text}`)
-    }
-  })
-})
+//   it('provides full width when block prop is given', async () => {
+//     await wrapper.setProps({ block: true })
+//     expect(wrapper.classes()).toContain('w-full')
+//   })
+//   it('renders sizes properly', async () => {
+//     const sizesAndClasses = {
+//       xs: {
+//         text: 'xs',
+//         px: 1.5,
+//         py: 1,
+//       },
+//       sm: {
+//         text: 'sm',
+//         px: 2,
+//         py: 1.5,
+//       },
+//       md: {
+//         text: 'base',
+//         px: 2.5,
+//         py: 2,
+//       },
+//       lg: {
+//         text: 'lg',
+//         px: 2.5,
+//         py: 2.5,
+//       },
+//       xl: {
+//         text: 'xl',
+//         px: 3,
+//         py: 3.5,
+//       },
+//     }
+//     for (const [key, value] of Object.entries(sizesAndClasses)) {
+//       await wrapper.setProps({ size: key })
+//       const classes = wrapper.find('[data-testid="lui-input-input"]').classes()
+//       expect(classes).toContain(`px-${value.px}`)
+//       expect(classes).toContain(`py-${value.py}`)
+//       expect(classes).toContain(`text-${value.text}`)
+//     }
+//   })
+// })
 
 describe('LuiInput HTML tests', () => {
   const testClass = 'ri-search-line'
@@ -168,107 +168,107 @@ describe('LuiInput HTML tests', () => {
   })
 })
 
-describe('LuiInput state', () => {
-  let wrapper: VueWrapper<any>
-  beforeEach(() => {
-    wrapper = mount(LuiInput, {
-      props: {
-        'description': testValue,
-        'state-icon': true,
-      },
-    })
-  })
-  afterEach(() => {
-    wrapper.unmount()
-  })
-  it('applies expected classes and renders proper icon when state prop is true', async () => {
-    await wrapper.setProps({ state: true })
-    // check the classes
-    expect(wrapper.find('[data-testid="lui-input-input"]').classes()).toContain(
-      'border-success-500',
-    )
-    expect(
-      wrapper.find('[data-testid="lui-input-description"]').classes(),
-    ).toContain('text-success-500')
-    // check if proper icon is exists
-    expect(wrapper.find('[data-testid="lui-input-state-true"]').exists()).toBe(
-      true,
-    )
-  })
-  it('applies expected classes and renders proper icon when state prop is warning', async () => {
-    await wrapper.setProps({ state: 'warning' })
-    // check the classes
-    expect(wrapper.find('[data-testid="lui-input-input"]').classes()).toContain(
-      'border-warning-500',
-    )
-    expect(
-      wrapper.find('[data-testid="lui-input-description"]').classes(),
-    ).toContain('text-warning-500')
-    // check if proper icon is exists
-    expect(
-      wrapper.find('[data-testid="lui-input-state-warning"]').exists(),
-    ).toBe(true)
-  })
-  it('applies expected classes and renders proper icon when state prop is false', async () => {
-    await wrapper.setProps({ state: false })
-    // check the classes
-    expect(wrapper.find('[data-testid="lui-input-input"]').classes()).toContain(
-      'border-danger-500',
-    )
-    expect(
-      wrapper.find('[data-testid="lui-input-description"]').classes(),
-    ).toContain('text-danger-500')
+// describe('LuiInput state', () => {
+//   let wrapper: VueWrapper<any>
+//   beforeEach(() => {
+//     wrapper = mount(LuiInput, {
+//       props: {
+//         'description': testValue,
+//         'state-icon': true,
+//       },
+//     })
+//   })
+//   afterEach(() => {
+//     wrapper.unmount()
+//   })
+//   it('applies expected classes and renders proper icon when state prop is true', async () => {
+//     await wrapper.setProps({ state: true })
+//     // check the classes
+//     expect(wrapper.find('[data-testid="lui-input-input"]').classes()).toContain(
+//       'border-success-500',
+//     )
+//     expect(
+//       wrapper.find('[data-testid="lui-input-description"]').classes(),
+//     ).toContain('text-success-500')
+//     // check if proper icon is exists
+//     expect(wrapper.find('[data-testid="lui-input-state-true"]').exists()).toBe(
+//       true,
+//     )
+//   })
+//   it('applies expected classes and renders proper icon when state prop is warning', async () => {
+//     await wrapper.setProps({ state: 'warning' })
+//     // check the classes
+//     expect(wrapper.find('[data-testid="lui-input-input"]').classes()).toContain(
+//       'border-warning-500',
+//     )
+//     expect(
+//       wrapper.find('[data-testid="lui-input-description"]').classes(),
+//     ).toContain('text-warning-500')
+//     // check if proper icon is exists
+//     expect(
+//       wrapper.find('[data-testid="lui-input-state-warning"]').exists(),
+//     ).toBe(true)
+//   })
+//   it('applies expected classes and renders proper icon when state prop is false', async () => {
+//     await wrapper.setProps({ state: false })
+//     // check the classes
+//     expect(wrapper.find('[data-testid="lui-input-input"]').classes()).toContain(
+//       'border-danger-500',
+//     )
+//     expect(
+//       wrapper.find('[data-testid="lui-input-description"]').classes(),
+//     ).toContain('text-danger-500')
 
-    // check if the proper icon is exists
-    expect(wrapper.find('[data-testid="lui-input-state-false"]').exists()).toBe(
-      true,
-    )
-  })
+//     // check if the proper icon is exists
+//     expect(wrapper.find('[data-testid="lui-input-state-false"]').exists()).toBe(
+//       true,
+//     )
+//   })
 
-  // it("renders icons and border based on the state", () => {
-  //   const trueWrapper = mount(LuiInput, {
-  //     props: {
-  //       description: testValue,
-  //       state: true,
-  //       "state-icon": true,
-  //     },
-  //   });
-  //   expect(
-  //     trueWrapper.find('[data-testid="lui-input-state-true"]').exists()
-  //   ).toBe(true);
-  //   expect(
-  //     trueWrapper.find('[data-testid="lui-input-input"]').classes()
-  //   ).toContain("border-success-500");
+//   // it("renders icons and border based on the state", () => {
+//   //   const trueWrapper = mount(LuiInput, {
+//   //     props: {
+//   //       description: testValue,
+//   //       state: true,
+//   //       "state-icon": true,
+//   //     },
+//   //   });
+//   //   expect(
+//   //     trueWrapper.find('[data-testid="lui-input-state-true"]').exists()
+//   //   ).toBe(true);
+//   //   expect(
+//   //     trueWrapper.find('[data-testid="lui-input-input"]').classes()
+//   //   ).toContain("border-success-500");
 
-  //   const warningWrapper = mount(LuiInput, {
-  //     props: {
-  //       description: testValue,
-  //       state: "warning",
-  //       "state-icon": true,
-  //     },
-  //   });
-  //   expect(
-  //     warningWrapper.find('[data-testid="lui-input-state-warning"]').exists()
-  //   ).toBe(true);
-  //   expect(
-  //     warningWrapper.find('[data-testid="lui-input-input"]').classes()
-  //   ).toContain("border-warning-500");
+//   //   const warningWrapper = mount(LuiInput, {
+//   //     props: {
+//   //       description: testValue,
+//   //       state: "warning",
+//   //       "state-icon": true,
+//   //     },
+//   //   });
+//   //   expect(
+//   //     warningWrapper.find('[data-testid="lui-input-state-warning"]').exists()
+//   //   ).toBe(true);
+//   //   expect(
+//   //     warningWrapper.find('[data-testid="lui-input-input"]').classes()
+//   //   ).toContain("border-warning-500");
 
-  //   const falseWrapper = mount(LuiInput, {
-  //     props: {
-  //       description: testValue,
-  //       state: false,
-  //       "state-icon": true,
-  //     },
-  //   });
-  // expect(
-  //   falseWrapper.find('[data-testid="lui-input-state-false"]').exists()
-  // ).toBe(true);
-  //   expect(
-  //     falseWrapper.find('[data-testid="lui-input-input"]').classes()
-  //   ).toContain("border-danger-500");
-  // });
-})
+//   //   const falseWrapper = mount(LuiInput, {
+//   //     props: {
+//   //       description: testValue,
+//   //       state: false,
+//   //       "state-icon": true,
+//   //     },
+//   //   });
+//   // expect(
+//   //   falseWrapper.find('[data-testid="lui-input-state-false"]').exists()
+//   // ).toBe(true);
+//   //   expect(
+//   //     falseWrapper.find('[data-testid="lui-input-input"]').classes()
+//   //   ).toContain("border-danger-500");
+//   // });
+// })
 
 // it("should render correctly based on the rounded prop", () => {
 //   // const roundedOptions = {
