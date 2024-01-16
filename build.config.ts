50ad2f47880d267fabcfb494b16c997c7dbf4de7
productdevbook <hi@productdevbook.com>
2024-01-16 09:05:46 +0300

    refactor: add Tailwind CSS plugin for CommonJS module format

--- build.config.ts
+++ build.config.ts
@@ -5,6 +5,7 @@ export default defineBuildConfig({
     { builder: 'mkdist', input: './src', pattern: ['**/*.css', '!**/*.stories.ts', '!tests/**'], loaders: ['sass'] },
     { builder: 'mkdist', input: './src', pattern: ['**/*.vue', '!**/*.stories.ts', '!tests/**'], loaders: ['vue'] },
     { builder: 'mkdist', input: './src', pattern: ['**/*.ts', '!**/*.stories.ts', '!tests/**'], format: 'esm', loaders: ['js'] },
+    { builder: 'mkdist', ext: 'cjs', format: 'cjs', input: 'src/tailwind/', outDir: 'dist/tailwind' },
   ],
   clean: true,
   declaration: true,
