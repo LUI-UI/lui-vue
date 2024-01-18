d138ee92d5472ec914391f3e774a8bfc1d3669a1
productdevbook <hi@productdevbook.com>
2024-01-18 14:34:19 +0300

    feat: add nuxt

--- packages/nuxt/src/module.ts
+++ packages/nuxt/src/module.ts
@@ -1,35 +1,61 @@
-import { addPlugin, createResolver, defineNuxtModule } from '@nuxt/kit'
+import { addComponent, addPlugin, createResolver, defineNuxtModule } from '@nuxt/kit'
 
 // Module options TypeScript interface definition
 export interface ModuleOptions {
-  apiKey: string
 }
 
-export interface ModuleHooks {
-  'my-module:init': any
-}
-
-export interface ModulePublicRuntimeConfig {
-  NAME: string
-}
-
-export interface ModuleRuntimeConfig {
-  PRIVATE_NAME: string
-}
+const luiComponents = [
+  'LuiAvatar',
+  'LuiBadge',
+  'LuiButton',
+  'LuiCheckbox',
+  'LuiMenuDropdown',
+  'LuiMenuItem',
+  'LuiInput',
+  'LuiRadio',
+  'LuiSelect',
+  'LuiMultiSelect',
+  'LuiOption',
+  'LuiSwitch',
+  'LuiTextarea',
+  'LuiModal',
+  'LuiTabGroup',
+  'LuiTabButtons',
+  'LuiTabButton',
+  'LuiTabPanels',
+  'LuiTabPanel',
+  'LuiTag',
+  'LuiPopover',
+  'LuiSidebar',
+]
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule',
+    name: 'lui-ui',
+    configKey: 'luiUI',
   },
   // Default configuration options of the Nuxt module
   defaults: {
-    apiKey: '',
   },
-  setup(_options, _nuxt) {
-    const resolver = createResolver(import.meta.url)
+  setup(_options, nuxt) {
+    // const resolver = createResolver(import.meta.url)
+
+    luiComponents.forEach((component) => {
+      addComponent({
+        name: component,
+        export: component,
+        filePath: '@lui-ui/lui-vue',
+      })
+    })
+
+    nuxt.options.vite.optimizeDeps = nuxt.options.vite.optimizeDeps || {}
+    nuxt.options.vite.optimizeDeps.include = nuxt.options.vite.optimizeDeps.include || []
+    nuxt.options.vite.optimizeDeps.include.push('@lui-ui/lui-vue')
+
+    nuxt.options.build.transpile = nuxt.options.build.transpile || []
+    nuxt.options.build.transpile.push('@lui-ui/lui-vue')
 
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
+    // addPlugin(resolver.resolve('./runtime/plugin'))
   },
 })
