8a0a922aaa9258932f38bf62ecfd48763c1221a6
rhmkstk <rahmi.kstk@gmail.com>
2024-02-02 12:10:39 +0300

    fix(client): fix menu anitmation bugs

--- packages/vue/src/components/MultiSelect/LuiMultiSelect.vue
+++ packages/vue/src/components/MultiSelect/LuiMultiSelect.vue
@@ -179,6 +179,7 @@ const { floatingStyles, middlewareData } = useFloating(wrapperRef, optionsWrappe
   placement: props.placement,
   middleware: [offset(6), flip(), shift()],
   whileElementsMounted: autoUpdate,
+  transform: false,
 })
 
 useOutsideClick(wrapperRef, () => {
@@ -706,7 +707,7 @@ const isOptionsActive = computed(() => optionsActive.value && !middlewareData.va
       <transition
         enter-active-class="transition duration-100 ease-out"
         enter-from-class="transform scale-95 opacity-0"
-        enter-to-class="transform scale-100 delay-75 opacity-100"
+        enter-to-class="transform scale-100 opacity-100"
         leave-active-class="transition duration-75 ease-in"
         leave-from-class="transform scale-100 opacity-100"
         leave-to-class="transform scale-95 opacity-0"
